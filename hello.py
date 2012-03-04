5ae5fbde2a967264d8615c9e39ceba2ff5f7c17b
Vinay Kumar <sdvinay@gmail.com>
2012-03-04 00:40:35 -0800

    Added playing time to the mix, to create a more complex eval

--- hello.py
+++ hello.py
@@ -4,26 +4,30 @@ from pyevolve import G1DList
 from pyevolve import GSimpleGA
 import csv
 
+playerPT = []
 playerScores = []
 playerNames = []
 
-f = open ("stats_01.csv", 'r')
+f = open ("stats_02.csv", 'r')
 statReader = csv.reader(f)
 for row in statReader:
-	playerScores.append(int(row[1]))
-	playerNames.append(row[2])
+	playerScores.append(int(row[3]))
+	playerPT.append(int(row[2]))
+	playerNames.append(row[4])
 	
 
 def eval_func(chromosome):
    score = 0.0
+   PT = 0.0
    # iterate over the chromosome
    for value in chromosome:
 #print value, playerScores[value]
-      score += playerScores[value]
-   return score
+      score += (playerScores[value] * playerPT[value])
+      PT += playerPT[value]
+   return score/PT
 
 genome = G1DList.G1DList(20)
-genome.setParams(rangemin=0, rangemax=1018)
+genome.setParams(rangemin=0, rangemax=299)
 genome.evaluator.set(eval_func)
 ga = GSimpleGA.GSimpleGA(genome)
 ga.evolve(freq_stats=10)
