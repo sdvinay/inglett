7c06ca3bdadb1c4a5fe822cd5b38453560961150
Vinay Kumar <sdvinay@gmail.com>
2012-03-05 23:49:31 -0800

    Code fixing: Mostly use of constants, more comments

--- pyevolve/hello.py
+++ pyevolve/hello.py
@@ -8,8 +8,16 @@ import csv
 import code
 import pyevolve
 
+# Lineup configuration
 positions = ['C', '1B', '2B', '3B', 'SS', 'LF', 'CF', 'RF']
+MINIMUM_PT = 4200
 
+# GA configuration
+MUTATION_RATE = .2
+POPULATION_SIZE = 1024
+NUM_GENERATIONS = 20000
+
+# Initialize the 2D-arrays that will keep the player/stat data
 playerPT = {}
 playerScores = {}
 playerNames = {}
@@ -19,6 +27,7 @@ for pos in positions:
 	playerScores[pos] = []
 	playerNames[pos] = []
 
+# read in the stat file, into the 2D array
 f = open ("../stats_03.csv", 'r')
 statReader = csv.reader(f)
 for row in statReader:
@@ -27,8 +36,9 @@ for row in statReader:
 	playerPT[pos].append(int(row[2]))
 	playerNames[pos].append(row[4])
 	
-#print playerNames
-
+# This function evaluates a lineup
+# It currently calc's aggregate OPS+ ("score", really), weighted by PT
+# Subject to MINIMUM_PT (any lineup that fails to meet MINIMUM_PT gets a score of 0)
 def eval_func(chromosome):
    score = 0.0
    PT = 0.0
@@ -38,11 +48,13 @@ def eval_func(chromosome):
       playerNum = int(chromosome[i])
       score += (playerScores[pos][playerNum] * playerPT[pos][playerNum])
       PT += playerPT[pos][playerNum]
+   if (PT < MINIMUM_PT):
+	  return 0
    return score/PT
 
-# For interactive mode
-	  # The generation that Pyevolve will enter on
-# the interactive mode
+# The following code is to enable interactive mode on OS X
+
+# The generation that Pyevolve will enter on the interactive mode
 INTERACTIVE_STOP = 100
  
 def evolve_callback(ga_engine):
@@ -68,9 +80,9 @@ genome.evaluator.set(eval_func)
 genome.mutator.set(Mutators.G1DListMutatorIntegerRange)
 genome.crossover.set(Crossovers.G1DListCrossoverUniform)
 ga = GSimpleGA.GSimpleGA(genome, interactiveMode=True)
-#ga.setPopulationSize(1024)
-ga.setGenerations(200)
-ga.setMutationRate(.1)
+ga.setPopulationSize(POPULATION_SIZE)
+ga.setGenerations(NUM_GENERATIONS)
+ga.setMutationRate(MUTATION_RATE)
 #ga.stepCallback.set(evolve_callback)
 ga.evolve(freq_stats=10)
 #print ga.bestIndividual().genomeList
